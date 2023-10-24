2610d1b3a5e8425447781bef1e958182914fe067
Egor Yakushev <TujhZreited@>
2023-10-24 10:33:40 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/сheckstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker.сheckstyle;
+
+public class Broken {
+
+    public static final String NEW_VALUE = "";
+    public String surname;
+    private int sizeOfEmpty = 10;
+
+    String name;
+
+    Broken() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    void echo() { }
+
+}
