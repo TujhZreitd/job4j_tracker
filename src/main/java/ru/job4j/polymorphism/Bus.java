35e7fc7aaa0389ab650cce888ac3064925a2c6a2
TujhZreitd <egor.yakushev.96@bk.ru>
2024-01-24 17:00:31 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.polymorphism;
+
+public class Bus implements Transport {
+
+    private int passengers;
+    private final double priceOil = 40.0;
+
+    @Override
+    public void drive() { }
+
+    @Override
+    public void passengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    @Override
+    public double refill(double oil) {
+        return oil * priceOil;
+    }
+}
