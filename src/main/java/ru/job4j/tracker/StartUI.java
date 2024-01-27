ce8e6e03409e0ef2bc683690c7f983d2585649b5
TujhZreitd <egor.yakushev.96@bk.ru>
2024-01-28 00:39:21 +0300

    6.1. Разрыв зависимости StartUI от Scanner. [#181778 #476164]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,7 +11,8 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu(actions);
-            int select = input.askInt("Выбрать: "); // коммент, чтобы засчитали урок
+            int select = input.askInt("Выбрать: ");
+            /* коммент, чтобы засчитали урок*/
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
