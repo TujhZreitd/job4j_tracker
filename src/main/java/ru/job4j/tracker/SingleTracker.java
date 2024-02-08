81e550d4cd3a6b903e8cb706928214c3db5355a8
TujhZreitd <egor.yakushev.96@bk.ru>
2024-02-08 23:05:00 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class SingleTracker {
+    private Tracker tracker = new Tracker();
+    private static SingleTracker singleton = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getSingleton() {
+        if (singleton == null) {
+            singleton = new SingleTracker();
+        }
+        return singleton;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public int indexOf(int id) {
+        return tracker.indexOf(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
