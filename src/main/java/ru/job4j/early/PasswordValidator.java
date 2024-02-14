af2f0f7f92cdfde1216f033a04985578b207f072
TujhZreitd <egor.yakushev.96@bk.ru>
2024-02-14 14:47:03 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #479988]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -5,25 +5,6 @@ import static java.lang.Character.*;
 public class PasswordValidator {
     private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
 
-    /**
-     * Метод проверят пароль по правилам:
-     *  1. Если password null, то выбросить исключение "Password can't be null";
-     *  2. Длина пароля находится в диапазоне [8, 32],
-     *     если нет то "Password should be length [8, 32]";
-     *  3. Пароль содержит хотя бы один символ в верхнем регистре,
-     *     если нет то "Password should contain at least one uppercase letter");
-     *  4. Пароль содержит хотя бы один символ в нижнем регистре,
-     *     если нет то "Password should contain at least one lowercase letter";
-     *  5. Пароль содержит хотя бы одну цифру,
-     *     если нет то"Password should contain at least one figure");
-     *  6. Пароль содержит хотя бы один спец. символ (не цифра и не буква),
-     *     если нет то "Password should contain at least one special symbol");
-     *  7. Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
-     *     Без учета регистра, значит что, например, ни qwerty ни QWERTY ни qwErty и т.п.
-     *     если да, то "Password shouldn't contain substrings: qwerty, 12345, password, admin, user".
-     * @param password Пароль
-     * @return Вернет пароль или выбросит исключение.
-     */
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException(
@@ -41,7 +22,6 @@ public class PasswordValidator {
         boolean hasDigit = false;
         boolean hasSpecial = false;
         for (char symbol : password.toCharArray()) {
-            /* Блок проверки принадлежности символа к определенной группе - Character.is ... */
             if (isUpperCase(symbol)) {
                 hasUpCase = true;
             }
@@ -53,6 +33,9 @@ public class PasswordValidator {
             }
             if (!isLetterOrDigit(symbol)) {
                 hasSpecial = true;
+            }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
             }
 
         }
