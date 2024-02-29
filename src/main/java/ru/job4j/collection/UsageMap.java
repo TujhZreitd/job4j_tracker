e4d267e5f2b3d125fd05d2c9af5fe1d1c2a72fc9
TujhZreitd <egor.yakushev.96@bk.ru>
2024-02-29 11:02:27 +0300

    0. Map, HashMap [#212652 #483908]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("egor.yakushev.96@bk.ru", "Yakushev Egor");
+        map.put("123456@mail.ru", "Ivanov Ivan");
+        map.put("sidorov@mail.ru", "Sidorov Anton");
+        map.put("123456@mail.ru", "Andreev Oleg");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
