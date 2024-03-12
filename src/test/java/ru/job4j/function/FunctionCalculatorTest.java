db7fd0937fd589f21c7c8b0c00e671e509cc388e
TujhZreitd <egor.yakushev.96@bk.ru>
2024-03-12 11:39:37 +0300

    6. Подсчет функции в диапазоне. [#24251 #485564]

--- src/test/java/ru/job4j/function/FunctionCalculatorTest.java
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -27,8 +27,8 @@ class FunctionCalculatorTest {
     @Test
     void whenIndicativeFunctionThenLinearResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(5, 8, x -> Math.pow(x, 3));
-        List<Double> expected = Arrays.asList(125D, 216D, 343D);
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(3, x));
+        List<Double> expected = Arrays.asList(243D, 729D, 2187D);
         assertThat(result).containsAll(expected);
     }
 }
